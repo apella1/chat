d6b3a10bf2231002c6eb9c9e0b5d2b4e8e6df4cb
apella1 <johncollons5@gmail.com>
2025-01-17 10:11:50 +0300

    fix: handle FileUtils error logging

--- src/main/kotlin/me/apella/chat/util/FileUtils.kt
+++ src/main/kotlin/me/apella/chat/util/FileUtils.kt
@@ -1,22 +1,31 @@
 package me.apella.chat.util
 
 import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import sun.jvm.hotspot.HelloWorld.e
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
 
 @Slf4j
 class FileUtils {
+    private val logger = LoggerFactory.getLogger(FileUtils::class.java)
     fun readFileFromLocation(fileUrl: String): ByteArray? {
         if (fileUrl.isBlank()) {
-            return byteArrayOf(0)
+            logger.error("File url is blank!")
+            return null
         }
 
         return try {
             val filePath = File(fileUrl).toPath()
+            if (!filePath.normalize().startsWith(File("./uploads").toPath())) {
+                logger.error("Attempted to access file outside allowed directory: $fileUrl")
+                return null
+            }
             Files.readAllBytes(filePath)
         } catch (e: IOException) {
-            return byteArrayOf(0)
+            logger.error("Failed to read file from path: $fileUrl", e)
+            null
         }
     }
 }
