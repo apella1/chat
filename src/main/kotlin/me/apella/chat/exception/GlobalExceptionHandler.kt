cd2190c9b72d9a5b180f17a9279009e6cfab484b
apella1 <johncollons5@gmail.com>
2025-01-17 11:11:54 +0300

    feat: handle global illegal argument and runtime exceptions

--- src/main/kotlin/me/apella/chat/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/me/apella/chat/exception/GlobalExceptionHandler.kt
@@ -1,3 +1,27 @@
 package me.apella.chat.exception
 
-class GlobalExceptionHandler
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import java.time.LocalDateTime
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(exception: IllegalArgumentException): CustomException {
+        return CustomException(
+            timestamp = LocalDateTime.now(),
+            status = HttpStatus.INTERNAL_SERVER_ERROR,
+            message = exception.message.toString()
+        )
+    }
+
+    @ExceptionHandler(RuntimeException::class)
+    fun handleRuntimeException(exception: RuntimeException): CustomException {
+        return CustomException(
+            timestamp = LocalDateTime.now(),
+            status = HttpStatus.INTERNAL_SERVER_ERROR,
+            message = exception.message.toString()
+        )
+    }
+}
