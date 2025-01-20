47b47ce04c502167e98fb3c6e4b0764392692053
apella1 <johncollons5@gmail.com>
2025-01-20 12:21:03 +0300

    fix: handle user synchronizer errors

--- src/main/kotlin/me/apella/chat/service/UserSynchronizer.kt
+++ src/main/kotlin/me/apella/chat/service/UserSynchronizer.kt
@@ -1,16 +1,20 @@
 package me.apella.chat.service
 
+import jakarta.transaction.Transactional
 import me.apella.chat.extension.fromTokenAttributes
 import me.apella.chat.repository.UserRepository
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Service
 
 @Service
-class UserSynchronizer (val userRepository: UserRepository) {
+class UserSynchronizer(val userRepository: UserRepository) {
+    @Transactional
     fun synchronizeWithIdp(token: Jwt) {
-        val userEmail = getUserEmail(token)
-        val optUser = userRepository.findUserByEmail(userEmail.toString())
+        val userEmail = getUserEmail(token) ?: throw IllegalArgumentException("Email not found in token!")
+        val optUser = userRepository.findUserByEmail(userEmail)
         val user = optUser?.fromTokenAttributes(token.claims)
+            ?: throw IllegalArgumentException("User not found for email: $userEmail")
+        userRepository.save(user)
     }
 
     private fun getUserEmail(token: Jwt): String? {
