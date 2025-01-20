b3bd173ffad97298c256064be136ab13129b9263
apella1 <johncollons5@gmail.com>
2025-01-20 12:30:30 +0300

    fix: update chat service error handling

--- src/main/kotlin/me/apella/chat/controller/ChatController.kt
+++ src/main/kotlin/me/apella/chat/controller/ChatController.kt
@@ -1,22 +1,32 @@
 package me.apella.chat.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import me.apella.chat.dto.ChatResponse
 import me.apella.chat.service.ChatService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/chats")
 @Tag(name = "Chats", description = "Endpoints related to the system chat.")
 class ChatController(val chatService: ChatService) {
+    @Operation(
+        summary = "Get user's chats",
+        description = "Retrieves a list of chats for the authenticated user."
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Chats retrieved successfully!"),
+        ApiResponse(responseCode = "401", description = "Unauthorized!"),
+        ApiResponse(responseCode = "500", description = "Internal server error!"),
+    ])
     @GetMapping
     fun getChatsByReceiver(authentication: Authentication): ResponseEntity<List<ChatResponse>> {
-        return ResponseEntity.ok(chatService.getChatsByReceiverId(authentication))
+        return ResponseEntity.ok(chatService.getChatsBySenderId(authentication))
     }
 }
--- src/main/kotlin/me/apella/chat/service/ChatService.kt
+++ src/main/kotlin/me/apella/chat/service/ChatService.kt
@@ -8,23 +8,17 @@ import me.apella.chat.repository.ChatRepository
 import me.apella.chat.repository.UserRepository
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class ChatService(val chatRepository: ChatRepository, val userRepository: UserRepository) {
     @Transactional
-    fun getChatsByReceiverId(currentUser: Authentication): List<ChatResponse> {
+    fun getChatsBySenderId(currentUser: Authentication): List<ChatResponse> {
         val userId = currentUser.name ?: throw IllegalArgumentException("User Id is required!")
+        val userUUID = userId.toUUID() ?: throw java.lang.IllegalArgumentException("Invalid UUID format for user Id!")
         return try {
-            chatRepository.findChatsBySenderId(userId.toUUID()!!).map { it.toResponseDTO(userId) }
+            chatRepository.findChatsBySenderId(userUUID).map { it.toResponseDTO(userId) }
         } catch (e: Exception) {
             throw RuntimeException("Failed to fetch chats: ", e)
         }
-    }
-
-    fun createChat(senderId: UUID, receiverId: UUID): String? {
-        val existingChat = chatRepository.findChatsBySenderIdAndRecipientId(senderId, receiverId)
-        val existingChatId = existingChat?.id
-        return existingChatId?.toString()
     }
 }
